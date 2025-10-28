0edb4ecfe0dfe59c21a9cac3635811b46abbaa76
Dalton Kajander <daltonkajander@yahoo.com>
2025-10-28 06:51:26 -0400

    More vectors

--- custom_math/Vector.py
+++ custom_math/Vector.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass, field
 import math
 
+PI = 3.141592654
+
 @dataclass
 class Vector2:
     x: float = 0
@@ -8,6 +10,9 @@ class Vector2:
     
     
     # Mathy methods
+    def __neg__(self) -> "Vector2": return Vector2(-self.x, -self.y)
+    def __bool__(self): return not (self.x == 0 and self.y == 0)
+    
     def __eq__(self, other: object) -> bool: 
         if (not isinstance(other, Vector2)): return NotImplemented
         return self.x == other.x and self.y == other.y
@@ -49,6 +54,33 @@ class Vector2:
     def dot(self, other: "Vector2") -> float:
         return (self.x * other.x) + (self.y * other.y)
     
+    def proj(self, other: "Vector2") -> "Vector2":
+        dot_prod = self.dot(other)
+        mag_sqr = self.magnitude() ** 2
+        div = dot_prod / mag_sqr
+        return Vector2(self.x * div, self.y * div)
+    
     # Non-math methods
-    def __bool__(self): return not (self.x == 0 and self.y == 0)
     def __str__(self): return f"<{self.x}, {self.y}>"
+    
+    
+    
+def get_2d_angle(v1: Vector2, v2: Vector2, mode = "rad") -> float:
+        # a.b = |a| * |b| * cos(theta)
+        # a.b / (|a| * |b|) = cos(theta)
+        # theta = arccos(a.b / (|a| * |b|))
+        
+        if (mode.lower() not in ["rad", "deg"]): return NotImplemented
+        
+        v1_mag = v1.magnitude()
+        v2_mag = v2.magnitude()
+        dot_prod = v1.dot(v2)
+        
+        if (dot_prod == (v1_mag * v2_mag)): # Angle is 0 deg
+            return 0
+        elif (dot_prod == -(v1_mag * v2_mag)): # Angle is 180 deg
+            return PI if mode == "rad" else 180
+        
+        rads = math.acos(dot_prod / (v1_mag * v2_mag))
+        
+        return rads if mode == "rad" else rads * 180 / PI
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from custom_math import *
 
 if __name__ == "__main__":
-    v1 = Vector2(4, 9)
-    v1.normalize()
-    print(v1)
+    v1 = Vector2(0, 4)
+    v2 = Vector2(4, 0)
+    
+    print(get_2d_angle(v1, v2))
+    print(get_2d_angle(v2, v1))
